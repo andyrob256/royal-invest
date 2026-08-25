b1f058ce767faba63c023f3879d524a7a420d409
andyrob256 <andrerobert256@.com>
2026-08-25 14:56:07 +0300

    Using MYSQL_URL only

--- server/db.js
+++ server/db.js
@@ -1,29 +1,8 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-console.log("DBHOST:", process.env.DBHOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_PORT:", process.env.DB_PORT);
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
-const pool = mysql.createPool({
-  host: process.env.DBHOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
-
-pool.getConnection()
-  .then(connection => {
-    console.log("✅ Database connected successfully!");
-    connection.release();
-  })
-  .catch(err => {
-    console.error("❌ Database connection failed:", err.message);
-  });
+console.log("✅ Database connected!");
 
 module.exports = pool;
